7ab66bdf6fc591dd0f8dc4fdd78e7fe76cda7ff5
Pratik Ramteke <pratikramteke222@gmail.com>
2023-05-07 19:08:35 +0530

    routes and api versioning done

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import routes from "./routes/index.js"
 
 const app = express()
 
@@ -8,5 +9,17 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use(cookieParser())
+
+app.use("/api/v1/",routes)
+app.get("/", (req, res) => {
+  res.send("Hello this is pratik API")
+})
+
+app.all('*', (req, res) => {
+  return res.status(404).json({
+    success: false,
+    message:"Route not found"
+  })
+})
 
 export default app
