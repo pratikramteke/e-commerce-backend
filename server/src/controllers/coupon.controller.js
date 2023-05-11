b55f0d04c806ce3cb9fabf0889c914d3a51c3ccc
Pratik Ramteke <pratikramteke222@gmail.com>
2023-05-11 19:43:25 +0530

    coupon controller done

--- server/src/controllers/coupon.controller.js
+++ server/src/controllers/coupon.controller.js
@@ -9,3 +9,81 @@ import CustomError from "../utils/customError.js"
  * @description Only admin and Moderator can create the coupon
  * @returns Coupon Object with success message "Coupon Created SuccessFully"
  *********************************************************/
+
+
+export const createCoupon = asyncHandler(async (req, res) => {
+  const { code, discount } = req.body
+
+  if (!code || !discount) {
+    throw new CustomError("Code and discount are required", 400)
+  }
+
+  // check id code already exists
+
+  const coupon = await Coupon.create({
+    code,
+    discount,
+  })
+
+  res.status(200).json({
+    success: true,
+    message: "Coupon created successfully",
+    coupon,
+  })
+})
+
+export const getAllCoupons = asyncHandler(async (req, res) => {
+  const allCoupons = await Coupon.find()
+
+  if (!allCoupons) {
+    throw new CustomError("No Coupons found", 400)
+  }
+
+  res.status(200).json({
+    success: true,
+    allCoupons,
+  })
+})
+
+export const updateCoupon = asyncHandler(async (req, res) => {
+    const {id: couponId} = req.params
+    const {action} = req.body
+
+    // action is boolean or not
+
+    const coupon = await Coupon.findByIdAndUpdate(
+        couponId,
+        {
+            active: action
+        }, 
+        {
+            new: true,
+            runValidators: true
+        }
+    )
+    if (!coupon) {
+        throw new CustomError("Coupon not found", 404)
+    }
+
+    res.status(200).json({
+        success: true,
+        message: "Coupon updated",
+        coupon
+    })
+})
+
+export const deleteCoupon = asyncHandler(async(req, res) => {
+    const {id: couponId} = req.params
+
+    const coupon = await Coupon.findByIdAndDelete(couponId)
+
+    if (!coupon) {
+        throw new CustomError("Coupon not found", 404)
+    }
+
+    res.status(200).json({
+        success: true,
+        message: "Coupon deleted",
+
+    })
+})
